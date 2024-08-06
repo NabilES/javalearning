3fe3624603b913975dac8c250d1244f976bed3bb
NabilES <nabiles9654@gmail.com>
2024-08-06 23:36:08 +0530

    feat: created a folder which contains person class and main class

--- constructor/Person.java
+++ constructor/Person.java
@@ -1,8 +1,18 @@
+package constructor;
+
 public class Person {
 
     private String name;
     private int age;
 
+    public Person(){
+        
+    }
+    
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName(){
         return name;
