12ade4912158b66557594515e5286b6f28a2fa96
NabilES <nabiles9654@gmail.com>
2024-08-06 23:19:04 +0530

    feat: created a main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        p.setName("Nabil");
+        p.setAge(21);
+
+        System.out.println("Name:" + p.getName());
+        System.out.println("Age: " + p.getAge());
+    }
+    
+}
